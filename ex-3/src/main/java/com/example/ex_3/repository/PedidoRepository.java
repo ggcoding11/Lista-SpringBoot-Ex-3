ca30f42535494dd7de78c6137893d4217759899f
JeeJ <gustavogomescoding@gmail.com>
2026-03-20 08:33:11 -0300

    add-repository

--- ex-3/src/main/java/com/example/ex_3/repository/PedidoRepository.java
+++ ex-3/src/main/java/com/example/ex_3/repository/PedidoRepository.java
@@ -3,5 +3,6 @@ package com.example.ex_3.repository;
 import com.example.ex_3.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Commit adicionado
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 }
